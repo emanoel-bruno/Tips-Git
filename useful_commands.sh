b25fc02abcfbc9c8c61c197c605870b8c3119259
Emanoel Bruno <emanoelbruno22@outlook.com>
2019-02-14 17:00:16 -0200

    Add Extension to files

---
+++ useful_commands.sh
@@ -0,0 +1,22 @@
+#/bin/bash
+
+# Create a empty git repository
+git init
+
+# Add the all files of current folder to state staged
+git add *
+
+# Add the a file to state staged
+git add <File name>
+
+# Tell status of working directory and staging area
+git status
+
+# Display commit history
+git log
+
+# Commit a file (Change from staged to commited)
+git commit -m "<Commit Name>"
+
+# Push changes to the remote repository
+git push <branch origin> <branch destiny>
