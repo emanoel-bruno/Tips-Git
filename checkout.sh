bd97e281e090e226b3cacb07bade0b07bd0321c8
Emanoel Bruno <emanoelbruno22@outlook.com>
2019-02-15 15:39:49 -0200

    Add New files

--- checkout.sh
+++ checkout.sh
@@ -3,5 +3,11 @@
 # Update all files to match the commit id
 git checkout <commit id>
 
-# Create a branch to remain changes on a "detached HEAD" and switch to it
+# Create a branch to remain changes made on a "detached HEAD state" and switch to it
 git checkout -b <New Branch Name>
+
+# Update a file to match the commit id
+git checkout <commit id> <filename>
+
+# Update a file to last commit id
+git checkout HEAD <filename>
