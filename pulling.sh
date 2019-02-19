25b643b4d303e4ff9e5fcbb8f32ed8bccbfb3811
Emanoel Bruno <emanoelbruno22@outlook.com>
2019-02-19 18:25:48 -0300

    Update pulling

--- pulling.sh
+++ pulling.sh
@@ -5,5 +5,8 @@
 # Update local project from the master on remote
 git pull origin master
 
-# Update local project from the master on remote
+# Update local project from the last commmit (HEAD)
+git pull 
+
+# Update local project from the branch on remote
 git pull <origin> <branch>
