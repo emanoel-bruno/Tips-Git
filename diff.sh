378d317f529b83cd3e2e8e0c66111561fc710631
Emanoel Bruno <emanoelbruno22@outlook.com>
2019-02-20 22:29:02 -0300

    Add Diff

--- diff.sh
+++ diff.sh
@@ -2,3 +2,18 @@
 
 # Show the diferences between the working directory and the staging area
 git diff
+
+# Show the diferences between the working directory and the last commit
+git diff HEAD
+
+# Show the diferences between the working directory and the commit id
+git diff <commit id>
+
+# Show the diferences between the staging area and the last commit
+git diff --staged HEAD
+
+# Show the diferences between a commit commit and the last
+git diff <commit id> HEAD
+
+# Show the diferences between two commit
+git diff <1º commit id> <2º commit id> 
