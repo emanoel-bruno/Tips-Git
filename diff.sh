02bfbb1d70fea845b41ad06b4628ecdb7db67cf2
Emanoel Bruno <emanoelbruno22@outlook.com>
2019-02-20 22:49:49 -0300

    Modified diff and remote

--- diff.sh
+++ diff.sh
@@ -18,5 +18,17 @@ git diff <commit id> HEAD
 # Show the diferences between two commit
 git diff <1º commit id> <2º commit id> 
 
+# Compare the local branch to the remote branch
+git diff <branch> origin/<branch> 
+
+# Compare local master to remote master
+git diff master origin/master
+
+# Compare local branch and master
+git diff <branch> master
+
+# Compare two local branch
+git diff <branch 1> <branch 2>
+
 # Show the diferences between one file and the staging area
 git diff -- <filename>
--- remote.sh
+++ remote.sh
@@ -2,3 +2,6 @@
 
 # List existings remotes using the git remote command
 git remote -v
+
+# Add a new remote address
+git remote add <name> <url>
